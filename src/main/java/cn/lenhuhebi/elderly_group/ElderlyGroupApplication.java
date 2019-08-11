9860e2ae30944652422a69b9687233381ec2cb40
Oblivion <xiang5liu@163.com>
2019-08-11 16:23:35 +0800

    testGit

---
+++ src/main/java/cn/lenhuhebi/elderly_group/ElderlyGroupApplication.java
@@ -0,0 +1,13 @@
+package cn.lenhuhebi.elderly_group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElderlyGroupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElderlyGroupApplication.class, args);
+    }
+
+}

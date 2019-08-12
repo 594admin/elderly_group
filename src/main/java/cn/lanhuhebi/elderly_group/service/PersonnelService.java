91e7ef14fbb24e926aa04df924a2427d58f0a189
Oblivion <xiang5liu@163.com>
2019-08-12 17:24:59 +0800

    模板 登录, 主页模板, 其他模板自己写自己的

---
+++ src/main/java/cn/lanhuhebi/elderly_group/service/PersonnelService.java
@@ -0,0 +1,10 @@
+package cn.lanhuhebi.elderly_group.service;
+
+/**
+ * @ClassName PersonnelService
+ * @Author Oblivion
+ * @Date 2019/8/12
+ * @Version 1.0
+ **/
+public interface PersonnelService {
+}
---
+++ src/main/java/cn/lanhuhebi/elderly_group/service/impl/PersonnelServiceImpl.java
@@ -0,0 +1,15 @@
+package cn.lanhuhebi.elderly_group.service.impl;
+
+import cn.lanhuhebi.elderly_group.service.PersonnelService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @ClassName PersonnelServiceImpl
+ * @Author Oblivion
+ * @Date 2019/8/12
+ * @Version 1.0
+ **/
+
+@Service
+public class PersonnelServiceImpl implements PersonnelService {
+}

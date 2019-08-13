3ab62a7a0e2532801ed8a88a971a43f9792c4372
liudewei <LiuDeWei@126.com>
2019-08-13 12:02:23 +0800

    添加户列表查看

---
+++ src/main/java/cn/lanhuhebi/elderly_group/dao/AreaDao.java
@@ -0,0 +1,13 @@
+package cn.lanhuhebi.elderly_group.dao;
+
+import cn.lanhuhebi.elderly_group.model.pojo.Area;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface AreaDao {
+    //查询所有的省
+    List<Area> queryAllSheng();
+    //根据上级id查询所有下级
+    List<Area> queryAllXiaByShang(@Param("areaid")Integer areaid);
+}

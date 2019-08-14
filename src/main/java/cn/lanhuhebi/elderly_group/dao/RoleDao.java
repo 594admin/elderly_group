09c003d67db054fa4d60854b172d92056b49e0e1
琚阳阳 <13360102@qq.com>
2019-08-14 21:59:38 +0800

    新增 ajax 判断修改用户 手机号与email 修改用户角色

---
+++ src/main/java/cn/lanhuhebi/elderly_group/dao/RoleDao.java
@@ -0,0 +1,12 @@
+package cn.lanhuhebi.elderly_group.dao;
+
+import java.util.List;
+
+public interface RoleDao {
+    /**
+     * 查询全部用户角色
+     * @return
+     */
+    List<RoleDao> getListRoles();
+
+}

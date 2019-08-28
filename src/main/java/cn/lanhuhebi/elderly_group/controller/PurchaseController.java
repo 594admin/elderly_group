e192a6bfb476ee2bbf8b0a67f12a43d75e2a7f20
Oblivion <xiang5liu@163.com>
2019-08-28 13:36:37 +0800

    Merge branch 'master' of https://github.com/594admin/elderly_group
    # Conflicts:
    #	src/main/resources/application.properties

--- src/main/java/cn/lanhuhebi/elderly_group/controller/PurchaseController.java
+++ src/main/java/cn/lanhuhebi/elderly_group/controller/PurchaseController.java
@@ -26,27 +26,27 @@ public class PurchaseController {
     private OrderService orderService;
 
 
-    @RequestMapping(value = "/doAddPurchase",method = RequestMethod.POST)
+    @RequestMapping(value = "/doAddPurchase", method = RequestMethod.POST)
     @Transactional
-    public String doAddPurchase(Purchase purchase, @RequestParam("orEptId") List<Integer> orEptId, @RequestParam("orEptNum")List<Integer> orEptNum, @RequestParam("orEptPrice")List<Double> orEptPrice , @RequestParam("purseFlyId")Integer purseFlyId , RedirectAttributes model){
-        System.out.println(orEptId+"*****************8");
+    public String doAddPurchase(Purchase purchase, @RequestParam("orEptId") List<Integer> orEptId, @RequestParam("orEptNum") List<Integer> orEptNum, @RequestParam("orEptPrice") List<Double> orEptPrice, @RequestParam("purseFlyId") Integer purseFlyId, RedirectAttributes model) {
+        System.out.println(orEptId + "*****************8");
         int i1 = this.purchaseService.addOnePurchase(purchase);
-        Integer or_purse_id=purchase.getPurseId();
-        Order order=new Order();
-        for(int i=0;i<orEptId.size();i++){
+        Integer or_purse_id = purchase.getPurseId();
+        Order order = new Order();
+        for (int i = 0; i < orEptId.size(); i++) {
             order.setOrEptId(orEptId.get(i));
             order.setOrEptNum(orEptNum.get(i));
-            order.setOrEptPrice((orEptPrice.get(i)*orEptNum.get(i)));
+            order.setOrEptPrice((orEptPrice.get(i) * orEptNum.get(i)));
             order.setOrPurseId(or_purse_id);
             this.orderService.addOrder(order);
         }
-        if(i1>0){
-            this.familyService.updateFamilyAddStatus(3,purseFlyId);
-            model.addAttribute("fly_status",3);
-            model.addAttribute("fly_id",purseFlyId);
+        if (i1 > 0) {
+            this.familyService.updateFamilyAddStatus(3, purseFlyId);
+            model.addAttribute("fly_status", 3);
+            model.addAttribute("fly_id", purseFlyId);
             return "redirect:/tosecond";
-        }else{
-            model.addAttribute("addmsg","添加失败");
+        } else {
+            model.addAttribute("addmsg", "添加失败");
             return "family/familyadd_equiment";
         }
 
